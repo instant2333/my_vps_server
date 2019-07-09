90da0d6847f3815a52375a16a5253ad1d2cc7ed2
instant2333 <39424709+instant2333@users.noreply.github.com>
2019-07-09 18:21:02 +0800

    v1.3(get multi coin)

--- myservice.py
+++ myservice.py
@@ -10,21 +10,15 @@ basedir=os.path.abspath(os.path.dirname(__file__))
 app=Flask(__name__)
 
 def getdata(symbol,period,size=1):
-  time1="symbol"+datetime.datetime.now().strftime('%Y-%m-%d %H.%M.%S')
-  filepath="static/"+time1+".json"
-  with open(filepath,'w+') as file_obj:
-    #json.dump(api.get_kline(symbol,period,size),file_obj)
-    json.dump({'1':filepath}, file_obj)
+  for temp in symbol:
+    time1=temp+datetime.datetime.now().strftime('%Y-%m-%d %H.%M.%S')
+    filepath="static/"+time1+".json"
+    with open(filepath,'w+') as file_obj:
+      json.dump(api.get_kline(symbol,period,size),file_obj)
+      #json.dump({'1':filepath}, file_obj)
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(getdata,'interval',seconds=120,args=["btcusdt","1min",3])
-scheduler.add_job(getdata,'interval',seconds=120,args=["ethusdt","1min",3])
-scheduler.add_job(getdata,'interval',seconds=120,args=["xrpusdt","1min",3])
-scheduler.add_job(getdata,'interval',seconds=120,args=["ltcusdt","1min",3])
-scheduler.add_job(getdata,'interval',seconds=120,args=["bchusdt","1min",3])
-scheduler.add_job(getdata,'interval',seconds=120,args=["eosusdt","1min",3])
-scheduler.add_job(getdata,'interval',seconds=120,args=["etcusdt","1min",3])
-scheduler.add_job(getdata,'interval',seconds=120,args=["adausdt","1min",3])
+scheduler.add_job(getdata,'interval',seconds=120,args=[["btcusdt","ethusdt","xrpusdt","ltcusdt","bchusdt","eosusdt","etcusdt","adausdt"],"1min",3])
 scheduler.start()
 
 if __name__ == "__main__":
